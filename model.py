4dd6a53136f364b87d0785af71c68281cf35c20d
JDekun <jurry_k@outlook.com>
2022-07-25 01:16:51 +0800

    local

--- model.py
+++ model.py
@@ -23,19 +23,13 @@ class Model(nn.Module):
         self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
                                nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
 
-    def covns(self, y):
-        y = self.f(y)
-        return y
-    
-    def mlps(self, y):
-        y = self.g(y)
-        return y
-
     def forward(self, x):
+        x = x + torch.zeros(1, dtype=x.dtype, device=x.device, requires_grad=True)
         if self.use_checkpoint:
-            x = checkpoint(self.covns, x)
+            print('use_checkpoint')
+            x = checkpoint(self.f, x)
             feature = torch.flatten(x, start_dim=1)
-            out = checkpoint(self.mlps, feature)
+            out = checkpoint(self.g, feature)
             return F.normalize(feature, dim=-1), F.normalize(out, dim=-1)
         else:
             x = self.f(x)
