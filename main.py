6837d6fcd1f97b24a89ae24cb2871b9436bf2a4c
JDekun <jurry_k@outlook.com>
2022-07-25 22:52:43 +0800

    local

--- main.py
+++ main.py
@@ -156,10 +156,10 @@ if __name__ == '__main__':
     parser.add_argument('--k', default=200, type=int, help='Top k most similar images used to predict the label')
     parser.add_argument('--batch_size', default=512, type=int, help='Number of images in each mini-batch')
     parser.add_argument('--epochs', default=500, type=int, help='Number of sweeps over the dataset to train')
+    # parser.add_argument('--amp', action='store_true', help='引用--amp时为True，没有引用时为False')
+    parser.add_argument('--amp', default=True, type=str2bool, help='amp')
     parser.add_argument('--results_path', default="results", type=str, help='results/')
     parser.add_argument('--datasets_path', default="../../input", type=str, help='../../input')
-    parser.add_argument('--amp', default=True, type=str2bool, help='amp')
-    # parser.add_argument('--amp', action='store_true', help='引用--amp时为True，没有引用时为False')
     parser.add_argument('--amp_level', default='O2', type=str, help='amp_level')
     parser.add_argument('--use_checkpoint', default=False, type=str2bool, help='use_checkpoint')
     parser.add_argument('--resume', default=False, type=str2bool, help='resume')
@@ -170,15 +170,17 @@ if __name__ == '__main__':
     # args parse
     args = parser.parse_args()
     feature_dim, temperature, k = args.feature_dim, args.temperature, args.k
-    path = args.results_path
-    path_d = args.datasets_path
     batch_size, epochs = args.batch_size, args.epochs
+
+    path, path_d = args.results_path, args.datasets_path
     use_checkpoint = args.use_checkpoint
     start_epoch = args.start_epoch
 
+
+    ###### seed ######
     set_seed(args.seed)
 
-    # wandb
+    ###### wandb ######
     wandb.init(project="simclr1")
     wandb.config.update(args)
 
@@ -191,6 +193,7 @@ if __name__ == '__main__':
 
     test_data = utils.CIFAR10Pair(root=path_d, train=False, transform=utils.test_transform, download=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+
 
     # model setup and optimizer config
     model = Model(feature_dim, use_checkpoint).cuda()
